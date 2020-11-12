103121a0ce0d9076a46aa7d35cc3038f7d1b23d5
chelseawowor <s21810201@student.unklab.ac.id>
2020-11-12 14:09:23 +0800

    edit pertemuan20+21+22

---
+++ Pertemuan20 (exersice#4)/src/OldApp.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+import Hello from './Components/Hello';
+import Content from './Components/Content';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hello />
+      <Content />7
+    </>
+  );
+};
+
+export default App;
